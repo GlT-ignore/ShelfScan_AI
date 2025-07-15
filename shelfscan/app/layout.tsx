ad1d4053700e2ec2649bcb38715dea4e5652d4cb
Chanikya Gajjarapu <gchanikya62@gmail.com>
2025-07-15 12:42:28 +0530

    feat: Add Vercel Analytics integration to app layout

--- shelfscan/app/layout.tsx
+++ shelfscan/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppProvider } from "../lib/context/AppContext";
 import StagewiseWrapper from "../components/StagewiseWrapper";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <AppProvider>
           {children}
         </AppProvider>
+        <Analytics />
         <StagewiseWrapper />
       </body>
     </html>
